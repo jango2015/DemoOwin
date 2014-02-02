114461651f08d09e030a5b31ebc100ea70e4f866
Lucian M <lucian.maran@outlook.com>
2014-02-02 18:54:13 +0200

    first commit

---
+++ InstallWebApiOwinIIS/Startup.cs
@@ -0,0 +1,18 @@
+﻿using InstallWebApiOwinIIS.Config;
+using Owin;
+using System.Web.Http;
+
+namespace InstallWebApiOwinIIS
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+ 
+            app.UseWebApi(config);
+        }
+    }
+}
